d2ca40672dd7095028236a98386e7d49477d30a4
ktarbet <ktarbet@gmail.com>
2015-02-03 15:44:00 -0700

    prep

---
+++ SetupAgriMetMetadata.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using System.Xml.XPath;
+
+
+
+namespace Shop
+{
+    class SetupAgriMetMetadata
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
